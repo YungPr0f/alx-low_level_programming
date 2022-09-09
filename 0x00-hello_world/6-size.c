760697425b6fae94ae88add2be9494aa70e1b15b
Yusuf <yusufomotoyosi@gmail.com>
2022-09-08 20:52:49 -0700

    C - Hello, World - 6. Size is not grandeur

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -20,5 +20,6 @@ int main(void)
 	printf("size of a long int: %lu byte(s)\n", sizeof(c));
 	printf("Size of a long long int: %lu byte(s)\n", sizeof(d));
 	printf("Size of a float: %lu byte(s)\n", sizeof(e));
+	fprintf(stderr, "%s", "Anything\n");
 	return (0);
 }
