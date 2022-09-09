605a8dc5b05215daa17ec1a3e1cb4ffedc455760
Yusuf <yusufomotoyosi@gmail.com>
2022-09-08 21:45:26 -0700

    C - Hello, World - 8. UNIX is basically a simple operating system

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,6 +8,7 @@
 
 int main(void)
 {
-	fwrite("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59, 1, stderr);
+	char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	fwrite(str, sizeof(str), 1, stderr);
 	return (1);
 }
