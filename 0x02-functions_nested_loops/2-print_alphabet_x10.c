aac8f8b4687f3e2eb5b03018aa2a6e1fceee14ea
Yusuf <yusufomotoyosi@gmail.com>
2022-09-13 15:25:52 -0700

    C - Functions, nested loops - 2. 10 x alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_alphabet - Alphabet game
+ * print_alphabet_x10 - Print the alphabets in lower case 10 times
  *
  * Return: void
  *
@@ -15,8 +15,8 @@ void print_alphabet_x10(void)
 	for (a = 0; a < 10; a++)
 	{
 		for (b = 97; b < 123; b++)
-                	_putchar(b);
+			_putchar(b);
 
-        	_putchar(10);
+		_putchar(10);
 	}
 }
