ce07fdca59eb277069256f8cfe3ff36da9d1f5d7
Yusuf <yusufomotoyosi@gmail.com>
2022-09-13 16:20:24 -0700

    C - Functions, nested loops - 3. islower

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -8,9 +8,9 @@
  *
  */
 
-int _islower(int c)
+int _isalpha(int c)
 {
-	if ((c >= 97 && c <= 122) || (c >= 41 && c <= 90))
+	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
 	{
 		return (1);
 	}
