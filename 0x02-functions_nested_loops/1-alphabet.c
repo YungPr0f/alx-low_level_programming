e117f529550cfe2d0305d168e00b1c1dc44a2d86
Yusuf <yusufomotoyosi@gmail.com>
2022-09-13 15:05:54 -0700

    C - Functions, nested loops - 1. Alphabet game

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,10 +1,17 @@
 #include "main.h"
 
+/**
+ * print_alphabet - Alphabet game
+ *
+ * Return: void
+ *
+ */
+
 void print_alphabet(void)
 {
 	int x;
 
-	for(x = 97; x < 123; x++)
+	for (x = 97; x < 123; x++)
 		_putchar(x);
 
 	_putchar(10);
