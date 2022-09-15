733c167709c0f429cf970918166a379f053843f1
Yusuf <yusufomotoyosi@gmail.com>
2022-09-15 11:40:22 -0700

    C - Debugging - 2. 0 > 972?

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -20,9 +20,13 @@ int largest_number(int a, int b, int c)
 	{
 		largest = b;
 	}
-	else if (c > b && c > a)
+	else if (c > a && c > b)
 	{
 		largest = c;
+	}
+	else
+	{
+	
 	}
 
 	return (largest);
