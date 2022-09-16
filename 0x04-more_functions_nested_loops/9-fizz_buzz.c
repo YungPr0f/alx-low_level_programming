68d2a38ddd28fad887f468f3af2f9d2210f7adbb
Yusuf <yusufomotoyosi@gmail.com>
2022-09-16 15:12:32 -0700

    More functions, more nested loops - 9. Fizz-Buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * main - prints the numbers from 1 to 100
+ * But for multiples of three, the number is replaced with "Fizz"
+ * for the multiples of five, the number is replaced with "Buzz"
+ * For multiples of both three and five print, replace with "FizzBuzz"
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 1; i < 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+			printf("FizzBuzz");
+		else if (i % 3 == 0)
+			printf("Fizz");
+		else if (i % 5 == 0)
+			printf("Buzz");
+		else
+			printf("%d", i);
+
+		if (i < 100)
+			printf(" ");
+	}
+
+	printf("\n");
+
+	return (0);
+}
