b171bbfa1c81a281ebc05db08ea1880bf9adffb4
Yusuf <yusufomotoyosi@gmail.com>
2022-09-15 21:02:23 -0700

    C - More functions, more nested loops - 5. universal language

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints the numbers, from 0 to 14, 10 times
+ */
+
+void more_numbers(void)
+{
+	int a;
+	int b;
+
+	for (a = 0; a < 10; a++)
+	{
+		for (b = 0; b < 15; b++)
+		{
+			_putchar(b + '0');
+		}
+	}
+
+	_putchar(10);
+}
