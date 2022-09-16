2716268a024a935464034f65ab9e2ad2f1d6c650
Yusuf <yusufomotoyosi@gmail.com>
2022-09-16 14:36:34 -0700

    More functions, more nested loops - 7. diagonal universe

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -9,15 +9,20 @@ void print_diagonal(int n)
 {
 	int i;
 	int j;
-
-	for (i = 0; i < n; i++)
+	
+	if (n > 0)
 	{
-		for (j = 0; j < i; j++)
-			_putchar(' ');
+		for (i = 0; i < n; i++)
+		{
+			for (j = 0; j < i; j++)
+				_putchar(' ');
 
-		_putchar(92);
-		_putchar('\n');
+			_putchar(92);
+			_putchar(10);
+		}
 	}
-
-	_putchar('\n');
+	else
+	{
+		_putchar(10);
+	}
 }
