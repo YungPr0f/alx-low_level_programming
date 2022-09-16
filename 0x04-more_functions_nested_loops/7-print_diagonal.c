86ffa39b6d696ec39be41ce1de86e4190ea09796
Yusuf <yusufomotoyosi@gmail.com>
2022-09-16 14:19:32 -0700

    More functions, more nested loops - 7. diagonal universe

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_diagonal - draws a diagonal line on the terminal.
+ * @n: number of times '\' is printed
+ */
+
+void print_diagonal(int n)
+{
+	int i;
+	int j;
+
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < i; j++)
+			_putchar(' ');
+
+		_putchar(92);
+		_putchar(10);
+	}
+}
