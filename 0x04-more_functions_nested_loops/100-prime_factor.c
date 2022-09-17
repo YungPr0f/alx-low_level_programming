a4215e1b9f48cb0f6b596205766abe2276e39850
Yusuf <yusufomotoyosi@gmail.com>
2022-09-17 00:55:01 -0700

    More functions, more nested loops - 11. Largest prime factor

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main - finds and prints the largest prime factor of the number 612852475143
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	long int n = 612852475143;
+	long int div = 2;
+	long int maxFact;
+
+	while(n != 0)
+	{
+		if (n % div != 0)
+			div = div + 1;
+		else
+		{
+			maxFact = n;
+			n = n / div;
+
+			if (n == 1)
+			{
+				printf("%ld\n", maxFact);
+				break;
+			}
+		}
+	}
+
+	return (0);
+}
