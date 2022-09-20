63c112b473ceb83800a5c81835887370366fc74a
Yusuf <yusufomotoyosi@gmail.com>
2022-09-20 14:53:14 -0700

    C - Pointers, arrays and strings - 4. Reverse gear

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string, in reverse
+ * @s: the string to reverse
+ */
+
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (count >= 0)
+	{
+		if (s[i] == '\0')
+			break;
+
+		i++;
+	}
+
+	for (i--; i >= 0; i--)
+		_putchar(s[i]);
+
+	_putchar(10);
+}
