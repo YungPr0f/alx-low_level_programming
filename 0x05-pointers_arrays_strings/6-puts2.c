d932ee3aaf612ce48eccaaf0f026effdeac89b1d
Yusuf <yusufomotoyosi@gmail.com>
2022-09-20 15:42:53 -0700

    C - Pointers, arrays and strings - 6. Half the lies

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every other character of a string,
+ * starting with the first character
+ * @str: the input string
+ */
+
+void puts2(char *str)
+{
+	int a = 0;
+
+	while (a >= 0)
+	{
+		if (str(a) == '\0')
+		{
+			_putchar(10);
+			break;
+		}
+
+		if (a % 2 == 0)
+			_putchar(str[a]);
+
+		a++;
+	}
+}
