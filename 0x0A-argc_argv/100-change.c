2986d0a4ea98504dfdbde642c2851d7137bc6f5d
Yusuf <yusufomotoyosi@gmail.com>
2022-09-30 20:52:39 -0700

    C - argc, argv - 5. Minimal Number of Coins for Change

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -33,7 +33,8 @@ int main(int argc, char *argv[])
 			else if (cents % 2 < cents)
 			{
 				cents -= 2;
-				coins++;			}
+				coins++;
+			}
 			else if (cents % 1 < cents)
 			{
 				cents -= 1;
@@ -48,5 +49,6 @@ int main(int argc, char *argv[])
 	}
 
 	printf("%d\n", coins);
+
 	return (0);
 }
