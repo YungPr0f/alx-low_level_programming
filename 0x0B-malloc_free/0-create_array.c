0967175663a685f43ff5e7ed7e89b907c2157959
Yusuf <yusufomotoyosi@gmail.com>
2022-10-03 11:04:16 -0700

    C - malloc, free - 0. Float like a butterfly

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 /**
- * create_array - creates an array of chars, and 
+ * create_array - creates an array of chars, and
  * initializes it with a specific char
  * @size: size of the array
  * @c: the array
