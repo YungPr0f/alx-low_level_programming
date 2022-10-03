c04942c119e8c86b9301c1b3b692ad8bbad37bb1
Yusuf <yusufomotoyosi@gmail.com>
2022-10-03 11:53:05 -0700

    C - malloc, free - 1. The woman who has no imagination has no wings

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,34 +1,38 @@
-#include "main.h"
 #include <stdlib.h>
 
 /**
- * _strdup - returns a pointer to a newly allocated space in memory,
- * which contains a copy of the string given as a parameter.
- * @str: the string to copy
- *
- * Return: NULL if str = NULL,  a pointer to the duplicated string,
- * or NULL if insufficient memory was available
- */
+* _strdup - Copies a string to new allocated space in memory
+*
+* @str: The string to be copied
+*
+* Return: Pointer to allocated memory containing copy of @str (SUCCESS)
+* OR NULL if @str is empty (FAILURE) OR
+* NUL if insufficient memory was available
+*/
 
 char *_strdup(char *str)
 {
-	char *copy;
-	int length;
+	char *alloc_mem;
+	unsigned int i;
+	unsigned int length_of_string = 0;
 
-	length = _strlen(char *str);
+	if (str == NULL)
+		return ('\0');
 
-	if (length == 0)
-	{
-		return (NULL);
-	}
-	else
-	{
-		copy = malloc(sizeof(char) * length);
-		if (copy == NULL)
-		{
-			return (NULL);
-		}
+	while (*(str + length_of_string) != '\0')
+		length_of_string++;
 
-		return _strcpy(char *copy, char *str, length);
-	}
+	length_of_string++;
+
+	alloc_mem = malloc(sizeof(*str) * length_of_string);
+
+	if (alloc_mem == NULL)
+		return ('\0');
+
+	for (i = 0; i < length_of_string; i++)
+		alloc_mem[i] = *(str + i);
+
+	alloc_mem[i] = '\0';
+
+	return (alloc_mem);
 }
