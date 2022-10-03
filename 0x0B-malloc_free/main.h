8ace4cc6672eed4cb06903928a0ac3f74b423fe2
Yusuf <yusufomotoyosi@gmail.com>
2022-10-03 12:42:15 -0700

    C - malloc, free - 2. courageous enough to take risks

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -3,8 +3,7 @@
 
 int _putchar(char c);
 char *create_array(unsigned int size, char c);
-int _strlen(char *s);
-char *_strcpy(char *dest, char *src, int n);
 char *_strdup(char *str);
+char *str_concat(char *s1, char *s2);
 
 #endif
