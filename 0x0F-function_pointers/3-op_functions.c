5a93acf4caa62673f673e9660796aa8bcd2c6ff7
Yusuf <yusufomotoyosi@gmail.com>
2022-10-12 13:56:19 -0700

    C - Function pointers - 3. A goal is something to aim at

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,27 +1,90 @@
-#ifndef OP_FUNCTIONS_
-#define OP_FUNCTIONS_
+#include <stdio.h>
+#include <stdlib.h>
+#include "3-calc.h"
 
 /**
-* struct op - Struct op
+* op_add - Adds two numbers @a and @b
 *
-* @op: The operator
+* @a: A number
 *
-* @f: Thefunction associated
+* @b: A number
+*
+* Return: Sum of @a and @b
 */
 
-typedef struct op
+int op_add(int a, int b)
 {
-	char *op;
-	int (*f)(int a, int b);
-} op_t;
+	return (a + b);
+}
 
+/**
+* op_sub - Calculates the difference of @c and @b
+*
+* @a: A number
+*
+* @b: A number
+*
+* Return: Difference of @a and @b
+*/
 
-int op_add(int a, int b);
-int op_sub(int a, int b);
-int op_mul(int a, int b);
-int op_div(int a, int b);
-int op_mod(int a, int b);
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
 
-int (*get_op_func(char *s))(int, int);
+/**
+* op_mul - Multiplies two numbers
+*
+* @a: A number
+*
+* @b: A number
+*
+* Return: Product of @a and @b
+*/
 
-#endif /* #ifndef OP_FUNCTIONS_ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+* op_div - Divides @a by @b
+*
+* @a: A number
+*
+* @b: A number
+*
+* Return: Result of the division of @a by @b without remainder
+*/
+
+int op_div(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	return (a / b);
+}
+
+/**
+* op_mod - Calculates the remainder of @a divided by @b
+*
+* @a: A number
+*
+* @b: A number
+*
+* Return: Remainder of @a divider by @b
+*/
+
+int op_mod(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	return (a % b);
+}
