ed9c68879a6f5b993246815f06d9b5fda38d56a1
Yusuf <yusufomotoyosi@gmail.com>
2022-10-27 14:05:36 -0700

    C - Bit manipulation - 0. 0

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,20 +1,10 @@
-#ifndef MAIN_H
-#define MAIN_H
-
-/*
- * File: holberton.h
- * AUth: Lateef O Yusuf
- * Desc: Header file containing prototypes for all functions
- *       written in the 0x14-bit_manipulation directory.
- */
+#ifndef BIT_MANIPULATION
+#define BIT_MANIPULATION
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
 
 int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
-void print_binary(unsigned long int n);
-int get_bit(unsigned long int n, unsigned int index);
-int set_bit(unsigned long int *n, unsigned int index);
-int clear_bit(unsigned long int *n, unsigned int index);
-unsigned int flip_bits(unsigned long int n, unsigned long int m);
-int get_endianness(void);
 
-#endif /* MAIN_H */
+#endif
